9edc55fbf76e5eb7b8c87863bb21734d910c9b64
Jacob <94486323+Jacobloon@users.noreply.github.com>
2023-12-11 19:19:52 -0600

    Delete components/PlayerDirections.java

--- components/PlayerDirections.java
+++
@@ -1,9 +0,0 @@
-package components;
-/**
- * Formatting method that might help player and enemy movement
- * @author Jacob Odenthal, Owen Talberg
- * @version 12/1/2023
- */
-public enum PlayerDirections {
-    UP, DOWN, LEFT, RIGHT
-}
